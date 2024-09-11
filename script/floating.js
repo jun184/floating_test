b9c64a891884d94ae5d00c0b9bea17e5134aaf4c
SJ <0616po@gmail.com>
2024-09-11 11:30:24 +0900

    신규 기능 추가
    var adType = "html" //이미지 'img', 대행사 iframe 코드 'html'
    var adUrl_img = "img/300x300_img.png"; //일반 이미지 배너 집행시
    var adUrl_html = "https://jun184.github.io/floating_test/adurl.html"; //대행사 iframe URL
    var adWidth = "300"; 
    var adHeight = "300";

--- script/floating.js
+++ script/floating.js
@@ -13,8 +13,8 @@ if(typeof btnRandom !== "undefined"){
     selected = 'LT'
 }
 
-if(typeof iframePB !== "undefined"){
-    if(iframePB == true && self != top){ //현재 iframe창 일 경우
+if(typeof iframePB !== "undefined" && iframePB == true){
+    if(self != top){ //현재 iframe창 일 경우
         adMarginLeft = "0"; 
         adMarginTop = "0";
         wrapperWidth = "0px"
@@ -35,17 +35,25 @@ if (selected == 'LT') { //Left top
     btnLeft = adWidth - (btnSizeWidth + btnMargin)
 }
 
-document.write ('<div id="all_wrapper1" style="position:absolute; left:0px; top:0px; width:'+wrapperWidth+'; height:0px; text-align:center; margin:0 auto; z-index:'+zIndex+';">\n');
-document.write ('<div id="second_wrapper1" style="position:relative; left:0px; top:0px; width:0px; height:0px; text-align:left; margin:0 auto;">\n');
-document.write ('<div id="FloatLayer1" style="position:absolute; left:'+adMarginLeft+'px; top:'+adMarginTop+'px; width:'+adWidth+'px; height:'+adHeight+'px; display:block; z-index:300;">\n');
+document.write('<div id="all_wrapper1" style="position:absolute; left:0px; top:0px; width:'+wrapperWidth+'; height:0px; text-align:center; margin:0 auto; z-index:'+zIndex+';">\n');
+document.write('<div id="second_wrapper1" style="position:relative; left:0px; top:0px; width:0px; height:0px; text-align:left; margin:0 auto;">\n');
+document.write('<div id="FloatLayer1" style="position:absolute; left:'+adMarginLeft+'px; top:'+adMarginTop+'px; width:'+adWidth+'px; height:'+adHeight+'px; display:block; z-index:300;">\n');
+if(adType=="html"){ //ifrmae 형태로 표현되는 광고
+    document.write('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+adWidth+'" HEIGHT="'+adHeight+'" SRC="'+adUrl_html+'" allowTransparency="true"></IFRAME>');
+}else{ // 일반 배너
+    document.write('<a href="//adv.khan.co.kr/RealMedia/ads/click_lx.ads/%%PAGE%%/%%RAND%%/%%POS%%/%%CAMP%%/%%IMAGE%%/%%USER%%" target="_blank">');
+    document.write('<img src="'+adUrl_img+'" width:'+adWidth+'px; height:'+adHeight+'px; border="0"></a>');
+}
+if(typeof iframePB !== "undefined" && iframePB == true){
+    if(self == top){ //현재창이 메인페이지인 경우에만 X버튼 적용
+        document.write('<div id="close" style="position:absolute; top:'+btnTop+'px; left:'+btnLeft+'px; display:block;">\n');
+        document.write('<a style="display: block;" href="javascript:floating_close();"><img src="https://advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg" width="'+btnSizeWidth+'" height="'+btnSizeHeight+'" border="0" style="display: block;"></a></div>\n');
+    }
+}
 
-document.write ('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+adWidth+'" HEIGHT="'+adHeight+'" SRC="'+adUrl+'" allowTransparency="true"></IFRAME>');
-document.write ('<div id="close" style="position:absolute; top:'+btnTop+'px; left:'+btnLeft+'px; display:block;">\n');
-document.write ('<a style="display: block;" href="javascript:floating_close();"><img src="https://advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg" width="'+btnSizeWidth+'" height="'+btnSizeHeight+'" border="0" style="display: block;"></a></div>\n');
-
-document.write ('</div>\n');
-document.write ('</div>\n');
-document.write ('</div>\n');
+document.write('</div>\n');
+document.write('</div>\n');
+document.write('</div>\n');
 
 function floating_close() {
     	document.getElementById('all_wrapper1').style.display = 'none'; 
